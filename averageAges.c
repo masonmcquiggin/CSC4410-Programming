2023a9739dedf10191b33937acbcb87c127940d5
masonmcquiggin <masonmcquiggin7@gmail.com>
2025-01-17 14:25:13 -0500

    Array Average Age
    This is an assignment given on January 17th, 2025 where using an array of given numbers (ages) find the the average of those numbers (ages).

---
+++ averageAges.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int ages[] = {20, 22, 18, 35, 48, 26, 87, 70};
+    int length = sizeof(ages) / sizeof(ages[0]); 
+    int sum = 0;
+    int i;
+
+    for (i = 0; i < length; i++) {
+        sum += ages[i];
+    }
+
+    float average = (float) sum / length;
+    printf("Average age: %.1f\n", average);
+
+    return 0;
+}
