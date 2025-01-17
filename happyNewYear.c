634f54c028ff816c1a2af4b821eea117118276d0
masonmcquiggin <masonmcquiggin7@gmail.com>
2025-01-17 13:50:32 -0500

    Happy New Years - While Loop
    This is the assignment given on January 17th, 2025 at the beginning of class. 
    Using a while loop count down from 10 to 0 and then print "Happy New Year"

---
+++ happyNewYear.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int n = 10;
+    
+    while (n >= 0) {
+        printf("%d\n", n);
+        n--;
+    }
+    printf("Happy New Year's!\n");
+    return 0;
+}
