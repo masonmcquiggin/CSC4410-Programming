83d967e94f4c9a0be1ef9a739899ad81dd50de01
masonmcquiggin <masonmcquiggin7@gmail.com>
2025-01-19 15:56:40 -0500

    Add Numbers using Pointers
    Write a C program to read two numbers from user and add
    them using pointers.

---
+++ PtrAddNumbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, sum;
+    int *ptr1, *ptr2; // Pointers to store addresses of numbers
+
+    // Read two numbers from the user
+    printf("Enter the first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter the second number: ");
+    scanf("%d", &num2);
+
+    // Assign addresses of num1 and num2 to pointers
+    ptr1 = &num1;
+    ptr2 = &num2;
+
+    // Add the values using pointers
+    sum = *ptr1 + *ptr2;
+
+    // Print the result
+    printf("The sum of %d and %d is %d.\n", *ptr1, *ptr2, sum);
+
+    return 0;
+}
