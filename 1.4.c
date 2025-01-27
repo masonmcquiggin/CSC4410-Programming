913a48928f7c6ca282a65dfb6baf025b031e1ae5
masonmcquiggin <masonmcquiggin7@gmail.com>
2025-01-27 14:07:42 -0500

    Calculate the percentage of a user's score in relation to the maximum score in a game

---
+++ 1.4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    float userScore, maxScore;
+
+    // Input the user's score and maximum score
+    printf("Enter user's score: ");
+    scanf("%f", &userScore);
+    printf("Enter maximum score: ");
+    scanf("%f", &maxScore);
+
+    // Calculate and display the percentage
+    if (maxScore > 0) {
+        printf("Percentage: %.2f%%\n", (userScore / maxScore) * 100);
+    } else {
+        printf("Maximum score must be greater than 0.\n");
+    }
+
+    return 0;
+}
