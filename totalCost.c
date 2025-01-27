b7517d3de6af7275079522b38ee76210eb4a6af3
masonmcquiggin <masonmcquiggin7@gmail.com>
2025-01-27 14:03:06 -0500

    1.3
    Calculate total cost of 50 items, each $9.99. Print out number of items, cost of each item, and the total cost (with 2 decimals)

---
+++ totalCost.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int itemAmount = 50;         
+    float itemCost = 9.99;  
+    double totalCost = itemAmount * itemCost; 
+
+    printf("Total Cost: %.2f\n", totalCost);
+}
